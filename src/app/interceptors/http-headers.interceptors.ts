affe7488cec4bdbe700f8b5a0bdf07a4132b6fe7
Katiasfihi <katia.sfihi@gmail.com>
2021-07-27 14:07:19 +0100

    importation and deleting params file

--- src/app/interceptors/http-headers.interceptors.ts
+++ src/app/interceptors/http-headers.interceptors.ts
@@ -1,3 +1,8 @@
+import { HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { HttpEvent } from "@angular/common/http";
+
 
 @Injectable()
 export class HttpErrorsInterceptor implements HttpInterceptor {
@@ -12,6 +17,11 @@ export class HttpErrorsInterceptor implements HttpInterceptor {
                 'x-rapidapi-key': 'esGbwrm390mshS2BCl0RALxQRtZTp1W7sFMjsnyJlJzDXVkW0H',
                 'x-rapidapi-host': 'rawg-video-games-database.p.rapidapi.com',
               },
-        })
+              setParams: {
+                 key: 'e40e743af2c94b0c916a8aa618fb4473',
+               }
+         });
+         return next.handle(req);
     }
 }
+        
